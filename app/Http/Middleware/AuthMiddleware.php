13ce60c2f1e41cc42ac36236442a0c8ae3d3c992
Naymur Rahman <naymurrahman@yahoo.com>
2025-01-25 20:43:29 +0600

    changed in middlewares

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Middleware;
 
+use App\Core\Authenticator;
+use App\Core\Session;
+
 class AuthMiddleware
 {
     public function handle()
     {
         if (!isset($_SESSION['user'])) {
-            redirect('/');
+            // Session::flash('flash_error', "Unauthorized!");
+
+            redirect('/login');
 
             // throw new \Exception("Unauthorized", 401);
+        }
+
+        if ($_SESSION['user']['status'] != 1) {
+            (new Authenticator)->logout();
+
+            redirect('/');
+
+            // throw new \Exception("Unauthorized! Invalid user!", 401);
         }
     }
 }
--- app/Http/Middleware/GuestMiddleware.php
+++ app/Http/Middleware/GuestMiddleware.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use App\Core\Session;
+
 class GuestMiddleware
 {
     public function handle()
     {
         if (isset($_SESSION['user'])) {
+            Session::flash('flash_error', "Access denied for authenticated users!");
+
             redirect('/');
 
             // throw new \Exception("Access denied for authenticated users.", 403);
