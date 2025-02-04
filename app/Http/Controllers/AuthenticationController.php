90bd59ac6fc33ea8b1937e83fef801207acf5fc0
Naymur Rahman <naymurrahman@yahoo.com>
2025-02-04 10:54:41 +0600

    updated authentication controller

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Core\Authenticator;
 use App\Core\Controller;
 use App\Core\Request;
+use App\Core\Session;
 use App\Models\User;
 
 class AuthenticationController extends Controller
@@ -16,7 +17,7 @@ class AuthenticationController extends Controller
      */
     public function index()
     {
-        view('pages.auth.login', array('title' => "Login"));
+        view('admin.pages.auth.login', array('title' => "Login"));
     }
 
     /**
@@ -71,7 +72,12 @@ class AuthenticationController extends Controller
             return redirect('/login');
         }
 
-        return redirect('/');
+        Session::flash('flash_success', "Login success.");
+
+        if ($_SESSION['user']['type'] == 3) {
+            redirect('/');
+        }
+        return redirect('/admin');
     }
 
     /**
@@ -85,5 +91,94 @@ class AuthenticationController extends Controller
         (new Authenticator)->logout();
 
         return redirect('/');
+    }
+
+
+    /**
+     * Register a user
+     *
+     * @return void
+     */
+    public function registerPage()
+    {
+        view('admin.pages.auth.register', array('title' => "Register"));
+    }
+
+    /**
+     * Store registered user
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function register(Request $request)
+    {
+        // Define sanitization rules
+        $request->setSanitizationRules([
+            'name' => ['string'],
+            'email' => ['email'],
+            'mobile' => ['string'],
+            'password' => ['string'],
+        ]);
+
+        // Validation rules
+        $rules = [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:127',
+            'mobile' => 'mobile|max:15',
+            'password' => 'required|string|min:8',
+        ];
+
+        // Validate data
+        $request->validate($rules);
+
+        $errors = $request->errors();
+
+        $errorFound = false;
+
+        if (!empty($errors)) {
+            $errorFound = true;
+        }
+
+        $email = $request->input('email');
+        if ($email != "") {
+            $usersModel = new User();
+            $user = $usersModel->where('email', '=', $email)->get();
+
+            if (count($user) > 0) {
+                $errorFound = true;
+                $errors['email'][] = "Email must be unique!";
+            }
+        }
+
+        if ($errorFound) {
+            // set errors and old data into session
+            $_SESSION['error'] = $errors;
+            $_SESSION['old'] = $request->all();
+
+            return redirect('/register');
+        }
+
+        $data = $request->validated();
+
+        $usersModel = new User();
+
+        $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+        if (isset($data['host']) && $data['host'] == 'on') {
+            $data['type'] = 2;
+            $data['status'] = 1;
+
+            unset($data['host']);
+        } else {
+            $data['type'] = 3;
+            $data['status'] = 1;
+        }
+        $data['created_at'] = date('Y-m-d H:i:s');
+        $data['updated_at'] = date('Y-m-d H:i:s');
+
+        $usersModel->insert($data);
+
+        Session::setPopup('popup_success', "Registration successfull. Please login to continue!");
+
+        return redirect('/login');
     }
 }
